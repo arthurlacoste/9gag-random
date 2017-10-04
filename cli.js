2b4ddccac9d78fde7c23866978610919055dca34
arthurlacoste <arthak@gmail.com>
2017-10-04 11:29:15 +0200

    Add an option to force the final name of the outpout file.

--- cli.js
+++ cli.js
@@ -5,6 +5,9 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+// if forceName is == '', generate different names
+const forceName = 'master'; // don't add .png ;)
+
 const dlImage = (imgUrl, imgName) => {
     let req = http.request(imgUrl, (res) => {
         let image = '';
@@ -21,6 +24,7 @@ const dlImage = (imgUrl, imgName) => {
 }
 
 const saveFile = (name, data) => {
+
     fs.writeFile(name, data, 'binary', (err) => {
         if (err) throw err;
         console.log(`Saved as ${name}`)
@@ -31,7 +35,11 @@ http.get({hostname: '9gag.com', port: 80, path: '/random', agent: false},
     (res) => {
         let imgUrl = url.parse(res.headers.location)['path'].split('/').pop()
         let imgName = imgUrl + '.png';
-
+	
+		if(forceName!='') {
+			imgName = forceName + '.png';
+		}
+        
         imgUrl = `http://img-9gag-fun.9cache.com/photo/${imgUrl}_700b.png`
         // let imgUrl_v1 = `http://img-9gag-fun.9cache.com/photo/${imgUrl}_700b_v1.png`
         dlImage(imgUrl, imgName);
